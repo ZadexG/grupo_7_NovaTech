627247705744290635b850c21e4d347ebd8c237f
unknown <urielwaigandt9@gmail.com>
2023-09-21 12:31:56 -0300

    register

--- src/app.js
+++ src/app.js
@@ -9,6 +9,11 @@ app.get("/", (req, res) => {
    
 })
 
+app.get("/register", (req, res) => {
+    res.sendFile (path.join(__dirname, "./views/register.html"));
+   
+})
+
 const port = 3030;
 app.listen(port, () => {
     console.log(`Servidor iniciado en http://localhost:${port}`);
