ecba7fce09d211f3d822fa25839b1b7b35685032
Ana Acuña <ana.081296@hotmail.com>
2023-10-03 12:28:38 -0300

    actualizacion estructura

--- src/app.js
+++ src/app.js
@@ -1,13 +1,15 @@
-const express = require ("express")
-const app = express()
-const path= require ("path")
+const express = require ('express');
+const app = express();
+const path = require ('path');
  
 const mainRoutes = require('./routes/main');
-app.set("view engine", "ejs")
-app.use(express.static(path.join(__dirname,"./public")))
-app.set ("views",path.join(__dirname,"views"))
 
-app.use("/", mainRoutes)
+app.use(express.static(path.join(__dirname, './public')));
+
+app.set('view engine', 'ejs');
+app.set ('views', path.join(__dirname, 'views'));
+
+app.use('/', mainRoutes);
 
 const port = 3030;
 app.listen(port, () => {
